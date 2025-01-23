70bff9712d6e0c1d1cf8de7fb20336dd077d50ea
Atharva2628 <atharva2628@protonmail.com>
2025-01-22 20:35:43 -0600

    added demo flatlist

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 import Colors from "../config/Colors";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-        {/* Temporary views */}
-      <View 
+      {/* Temporary views */}
+      <View
         style={{
           flexDirection: "row",
           padding: 12,
@@ -26,6 +26,77 @@ export default function Home() {
           </Text>
         </View>
       </View>
+
+      {/* Flatlist */}
+      <FlatList
+        data={[
+          {
+            key: 0,
+            title: "What is the meaning of life?",
+            text: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            user_pfp: require("../assets/pfp-1.jpg"),
+          },
+          {
+            key: 1,
+            title: "Are we living in a simulation?",
+            text: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            user_pfp: require("../assets/pfp-2.jpg"),
+          },
+          {
+            key: 2,
+            title:
+              "How many licks does it take to get to the center of a tootsie pop?",
+            text: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            user_pfp: require("../assets/pfp-3.jpg"),
+        },
+          {
+            key: 3,
+            title: "How many humans can survive on Mars?",
+            text: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            user_pfp: require("../assets/pfp-4.jpg"),
+        },
+        {
+            key: 4,
+            title: "Where is the best place to live in the world?",
+            text: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            user_pfp: require("../assets/pfp-5.jpg"),
+        },
+        {
+            key: 5,
+            title: "What is the best way to learn a new language?",
+            text: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+            user_pfp: require("../assets/pfp-6.jpg"),
+        },
+        ]}
+        renderItem={({ item }) => (
+          <View style={styles.conversationContainer}>
+            <Text style={styles.highlightText}>{item.title}</Text>
+
+            <View
+              style={{
+                flexDirection: "row",
+                padding: 12,
+                alignItems: "center",
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              <Image
+                style={{
+                  width: 32,
+                  height: 32,
+                  borderRadius: 50,
+                  //   marginRight: 12,
+                }}
+                source={item.user_pfp}
+              />
+              <View style={{ width: "85%", alignItems: "center" }}>
+                <Text style={styles.text}>{item.text}</Text>
+              </View>
+            </View>
+          </View>
+        )}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -36,7 +107,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.primary,
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
+    padding: 12,
   },
   title: {
     fontSize: 24,
@@ -51,5 +123,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: Colors.accent,
     fontWeight: "bold",
+  },
+  conversationContainer: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.secondary,
   },
 });
